4f98a2626c7fa7d024d2c06ca648831c5b2567ee
Nico <richard.nicolas44@gmail.com>
2025-04-20 13:32:54 +0200

    mise a jour version spring boot et suppression import inutilise

--- task-management-back/src/main/java/fr/natixis/api/TaskController.java
+++ task-management-back/src/main/java/fr/natixis/api/TaskController.java
@@ -1,7 +1,5 @@
 package fr.natixis.api;
 
-import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
